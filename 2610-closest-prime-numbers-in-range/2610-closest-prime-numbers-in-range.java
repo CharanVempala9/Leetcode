c9f514d8cad19de80c4509c0aebfad5e16ccc078
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-03-07 19:23:40 +0530

    Time: 91 ms (68.4%), Space: 50.2 MB (43.6%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -1,34 +1,33 @@
 class Solution {
-    public boolean isprime(int n) {
-    if (n <= 1) return false;  
-    if (n == 2 || n == 3) return true;  
-    if (n % 2 == 0 || n % 3 == 0) return false;  
-    for (int i = 5; i * i <= n; i += 6) {  
-        if (n % i == 0 || n % (i + 2) == 0)  
-            return false;  
-        }
-    return true;
-}
-
     public int[] closestPrimes(int left, int right) {
+        boolean[] primes=new boolean[right+1];
+        Arrays.fill(primes,true);
+        primes[0]=primes[1]=false;
+        for(int i=2; i*i<=right; i++){
+            if(primes[i]){
+                for(int j=i*i; j<=right; j+=i){
+                    primes[j]=false;
+                }
+            }
+        }
         ArrayList<Integer>ls=new ArrayList<>();
         for(int i=left; i<=right; i++){
-            if(isprime(i)){
+            if(primes[i]){
                 ls.add(i);
             }
         }
-        int[] res=new int[2];
-        int ans=Integer.MAX_VALUE;
         if(ls.size()<2){
             return new int[]{-1,-1};
         }
+        int[] res=new int[2];
+        int ans=Integer.MAX_VALUE;
         for(int i=1; i<ls.size(); i++){
-                int temp=Math.abs(ls.get(i)-ls.get(i-1));
-                if(temp<ans){
-                    ans=temp;
-                    res[0]=ls.get(i-1);
-                    res[1]=ls.get(i);
-                }
+            int temp=Math.abs(ls.get(i-1)-ls.get(i));
+            if(temp<ans){
+                ans=temp;
+                res[0]=ls.get(i-1);
+                res[1]=ls.get(i);
+            }
         }
         return res;
     }
