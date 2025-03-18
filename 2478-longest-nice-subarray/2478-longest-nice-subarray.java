36fcef19ef31e9cb77a0f2858025ca85997dc5b2
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-03-18 06:52:15 +0530

    Time: 3 ms (98.64%), Space: 56.1 MB (23.43%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.java
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.java
@@ -1,35 +1,16 @@
 class Solution {
     public int longestNiceSubarray(int[] nums) {
         int l = 0;
-        int n = nums.length;
-        int max = 1; 
-
-        for (int r = 1; r < n; r++) {
-            boolean conflict = false;
-            for (int k = l; k < r; k++) {
-                if ((nums[k] & nums[r]) != 0) {
-                    conflict = true;
-                    break;
-                }
+        int currMask = 0;
+        int max = 0;
+        for (int r = 0; r < nums.length; r++) {
+            while ((currMask & nums[r]) != 0) {
+                currMask ^= nums[l]; 
+                l++;
             }
-
-            if (conflict) {
-                while (l < r) {
-                    boolean foundConflict = false;
-                    for (int k = l + 1; k < r; k++) {
-                        if ((nums[k] & nums[r]) != 0) {
-                            foundConflict = true;
-                            break;
-                        }
-                    }
-                    l++;
-                    if (!foundConflict) break;
-                }
-            }
-
+            currMask |= nums[r];
             max = Math.max(max, r - l + 1);
         }
-
         return max;
     }
 }
