2ff14ab7786fcbb27ed8d65738ae0d999e23c53d
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-03-19 18:23:04 +0530

    Time: 8 ms (34.91%), Space: 57.4 MB (61.4%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -1,32 +1,25 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int l=0;
         int n=nums.length;
-        int k=3;
-        int ans=0;
-        int sum=0;
-        for(int r = 0; r < n; r++) {
-        sum += nums[r];
-        if (r - l + 1 == k) {
-            if (nums[l] == 0 && sum < 3) {
-                for (int idx = l; idx <= r; idx++) {
-                if (nums[idx] == 0) nums[idx] = 1;
-                else nums[idx] = 0;
-                }
-                ans++;
-                sum = 0;
-                for (int idx = l; idx <= r; idx++) sum += nums[idx];
+        int cnt=0;
+        for(int i=0; i<n-2; i++){
+            if(nums[i]==0){
+                flip(nums,i);
+                cnt++;
             }
-            sum -= nums[l];
-            l++;
         }
-        }
-        for(int i=0;i<n; i++){
+        for(int i=0; i<n; i++){
             if(nums[i]!=1)
             return -1;
-
         }
-        return ans;
-
+        return cnt;
+    }
+    public void flip(int[] nums,int idx){
+        for(int i=idx; i<idx+3; i++){
+            if(nums[i]==0)
+            nums[i]=1;
+            else
+            nums[i]=0;
+        }
     }
 }
