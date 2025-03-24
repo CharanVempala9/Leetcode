4a833e5bc7e135cb813c84094d0324602402ca7e
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-03-24 21:10:42 +0530

    Time: 40 ms (48.82%), Space: 100.7 MB (41.41%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.java
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.java
@@ -1,29 +1,21 @@
-import java.util.*;
-
 class Solution {
     public int countDays(int days, int[][] meetings) {
-        List<int[]> events = new ArrayList<>();
-        for (int[] m : meetings) {
-            events.add(new int[]{m[0], 1});  
-            events.add(new int[]{m[1] + 1, -1});
-        }
-        events.sort((a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
         
-        int ongoing = 0;
-        int lastDay = 0;
-        int meetingDays = 0;
+        int result = 0;
+        int end = 0;
         
-        for (int[] event : events) {
-            int day = event[0];
-            int type = event[1];
-            
-            if (ongoing > 0) {
-                meetingDays += day - lastDay;
+        for (int[] meet : meetings) {
+            if (meet[0] > end) {
+                result += meet[0] - end - 1;
             }
-            ongoing += type;
-            lastDay = day;
+            end = Math.max(end, meet[1]);
         }
         
-        return days - meetingDays;
+        if (days > end) {
+            result += days - end;
+        }
+        
+        return result;
     }
 }
