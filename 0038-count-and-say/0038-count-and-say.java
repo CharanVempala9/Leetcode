2830e271665b4ad9a64ebcd5c1f1718c2d5ed52f
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-04-18 16:47:07 +0530

    Time: 6 ms (49.76%), Space: 41.3 MB (79.02%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,33 +1,26 @@
 class Solution {
     public String countAndSay(int n) {
-        if (n == 1)
-            return "1";
-        int idx = 2;
-        StringBuilder res = new StringBuilder();
-        res.append("1");
-        while (idx <= n) {
-            StringBuilder temp = new StringBuilder();
-            LinkedHashMap<Character, Integer> hm = new LinkedHashMap<>();
-            for (int i = 0; i < res.length(); i++) {
-                char ch = res.charAt(i);
-                if (hm.containsKey(ch)) {
-                    hm.put(ch, hm.get(ch) + 1);
-                } else {
-                    for (char key : hm.keySet()) {
-                        temp.append(hm.get(key));
-                        temp.append(key);
-                    }
-                    hm.clear();
-                    hm.put(ch, 1);
+        String curr = "1";
+        if(n==1) return curr;
+        for(int i=2;i<=n;i++)
+        {
+            StringBuilder sb = new StringBuilder();
+            int cnt = 1;
+            char ele = curr.charAt(0);
+            for(int j=1;j<curr.length();j++)
+            {
+                char currChar = curr.charAt(j);
+                if(currChar == ele) 
+                cnt++;
+                else{
+                    sb.append(cnt).append(ele);
+                    ele = currChar;
+                    cnt=1;
                 }
             }
-            for (char key : hm.keySet()) {
-                temp.append(hm.get(key));
-                temp.append(key);
-            }
-            res = temp;
-            idx++;
+            sb.append(cnt).append(ele);
+            curr = sb.toString();
         }
-        return res.toString();
+        return curr;
     }
 }
