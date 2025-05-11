c1d7a3799075bcf8ca189aef9868b51062019f3b
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-05-11 13:43:01 +0530

    Time: 0 ms (100%), Space: 42.5 MB (43.42%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.java
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
-        int n=arr.length;
-        for(int i=1; i<n-1; i++){
-            if(arr[i-1]%2==1 && arr[i]%2==1 && arr[i+1]%2==1)
-            return true;
+        if (arr.length < 3) return false;
+        for (int i = 0; i <= arr.length - 3; i++) {
+            if ((arr[i] & 1) == 1 && (arr[i+1] & 1) == 1 && (arr[i+2] & 1) == 1)
+                return true;
         }
         return false;
     }
