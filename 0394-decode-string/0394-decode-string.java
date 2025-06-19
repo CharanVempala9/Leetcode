caa4e9e8f0b76772b55ce802d7085cdc909f98a9
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-06-19 11:22:06 +0530

    Time: 1 ms (68.06%), Space: 41.4 MB (81.2%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,34 +1,35 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Character> st = new Stack<>();
-        
-        for (char ch : s.toCharArray()) {
-            if (ch != ']') {
-                st.push(ch);
-            } else {
-                StringBuilder temp = new StringBuilder();
-                while (!st.isEmpty() && st.peek() != '[') {
-                    temp.insert(0, st.pop()); 
+        StringBuilder curr = new StringBuilder();
+        Stack<StringBuilder> stack = new Stack<>();
+        Stack<Integer> countStack = new Stack<>();
+        int k = 0;
+
+        for (char c: s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                k = k*10 + (c-'0');
+                continue;
+            }
+            if (Character.isLetter(c)) {
+                curr.append(c);
+                continue;
+            }
+            if (c == '[') {
+                stack.push(curr);
+                countStack.push(k);
+                k = 0;
+                curr = new StringBuilder();
+            }
+            if (c == ']') {
+                int times = countStack.pop();
+                StringBuilder prefix = stack.pop();
+                while (times>0) {
+                    prefix.append(curr);
+                    times--;
                 }
-                st.pop(); 
-                
-                StringBuilder numStr = new StringBuilder();
-                while (!st.isEmpty() && Character.isDigit(st.peek())) {
-                    numStr.insert(0, st.pop());
-                }
-                int num = Integer.parseInt(numStr.toString());
-                
-                String repeated = temp.toString().repeat(num);
-                for (char c : repeated.toCharArray()) {
-                    st.push(c);
-                }
+                curr = prefix;
             }
         }
-        StringBuilder res = new StringBuilder();
-        while (!st.isEmpty()) {
-            res.insert(0, st.pop());
-        }
-        
-        return res.toString();
+        return curr.toString();
     }
 }
