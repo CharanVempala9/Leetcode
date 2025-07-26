bac30f191423b8567152fc9b4777a9ecf44c5347
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-07-26 18:30:07 +0530

    Time: 87 ms (26.89%), Space: 57.6 MB (22.18%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,17 +1,26 @@
 class Solution {
-    public boolean canPartition(int[] nums) {
-        int totalSum = 0;
-        for (int num : nums) totalSum += num;
-        if (totalSum % 2 != 0) return false;
-        int targetSum = totalSum / 2;
-        boolean[] dp = new boolean[targetSum + 1];
-        dp[0] = true;
-        for (int num : nums) {
-            for (int currSum = targetSum; currSum >= num; currSum--) {
-                dp[currSum] = dp[currSum] || dp[currSum - num];
-                if (dp[targetSum]) return true;
-            }
+    public boolean isposs(int[] nums,int i,int sum, Boolean[][]dp){
+        if(sum==0) return true;
+        if(i==0){
+            return (nums[0]==sum);
         }
-        return dp[targetSum];
+        if(dp[i][sum]!=null) return dp[i][sum];
+        boolean notTake=isposs(nums,i-1,sum,dp);
+        boolean take=false;
+        if(nums[i]<=sum){
+            take=isposs(nums,i-1,sum-nums[i],dp);
+        }
+        dp[i][sum] = notTake || take;
+        return dp[i][sum];
+    }
+    public boolean canPartition(int[] nums) {
+        int n=nums.length;
+        int totSum=0;
+        for(int i=0; i<n; i++){
+            totSum+=nums[i];
+        }
+        if(totSum%2==1) return false;
+        Boolean[][] dp=new Boolean[n][(totSum/2)+1];
+        return isposs(nums,n-1,totSum/2,dp);
     }
 }
