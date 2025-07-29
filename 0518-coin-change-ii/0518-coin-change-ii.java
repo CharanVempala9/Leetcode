b6348c4815a54ddb175fefbe619062fb75364832
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-07-29 16:40:02 +0530

    Time: 8 ms (70.01%), Space: 44.8 MB (68.16%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,23 +1,25 @@
 class Solution {
-    public int  gen(int idx, int[] coins, int amount,int[][] dp){
-        if(idx==0){
-            if(amount==0) return 1;
-            if(amount % coins[0]== 0) return 1;
-            return 0;
-        }
-        if(dp[idx][amount]!=-1) return dp[idx][amount];
-        int take=0;
-        if(coins[idx]<=amount){
-            take=gen(idx,coins,amount-coins[idx],dp);
-        }
-        int notTake=gen(idx-1, coins,amount,dp);
-        dp[idx][amount]= take+notTake;
-        return dp[idx][amount];
-    }
     public int change(int amount, int[] coins) {
-        int n=coins.length;
-        int[][] dp=new int[n][amount+1];
-        for(int i=0; i<n; i++) Arrays.fill(dp[i],-1);
-        return gen(n-1, coins,amount,dp);
+        int n = coins.length;
+        int[] prev = new int[amount + 1];
+
+        for (int a = 0; a <= amount; a++) {
+            if (a % coins[0] == 0)
+                prev[a] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            int[] curr = new int[amount + 1];
+            for (int a = 0; a <= amount; a++) {
+                int notTake = prev[a];
+                int take = 0;
+                if (coins[i] <= a)
+                    take = curr[a - coins[i]];
+                curr[a] = take + notTake;
+            }
+            prev = curr;
+        }
+
+        return prev[amount];
     }
 }
