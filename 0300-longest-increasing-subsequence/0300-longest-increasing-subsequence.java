f685717f4c02a4b29fc206e3485f12148386fe5f
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-08-13 19:50:59 +0530

    Time: 7 ms (80.76%), Space: 44.3 MB (66.92%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,25 +1,22 @@
 class Solution {
-    public int rec(int idx, int prev,int[] nums, int[][] dp){
-        if(idx==nums.length){
-            return 0;
-        }
-        if(dp[idx][prev+1]!=-1) return dp[idx][prev+1];
-        int take=0;
-        int notTake=0;
-        if(prev == -1 || nums[idx] > nums[prev]){
-            take = 1 + rec(idx+1, idx , nums,dp);
-        }
-        notTake = rec(idx+1, prev , nums, dp);
-        dp[idx][prev+1] = Math.max(take, notTake);
-        return dp[idx][prev+1];
-
-    }
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
-        int[][] dp=new int[n][n+1];
-        for(int i=0; i<n; i++){
-            Arrays.fill(dp[i],-1);
+        List<Integer>ls=new ArrayList<>();
+        ls.add(nums[0]);
+        int ans=1;
+        for(int i=1; i<n; i++){
+            if(nums[i] > ls.get(ls.size()-1)){
+                ls.add(nums[i]);
+                ans++;
+            }
+            else{
+                int idx = Collections.binarySearch(ls, nums[i]);
+                if(idx<0){
+                    idx = -idx-1;
+                }
+                ls.set(idx,nums[i]);
+            }
         }
-        return rec(0,-1,nums,dp);
+        return ans;
     }
 }
