6059ff48ac8bae6b4aad28e3210e1655ddc6e099
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-08-30 15:59:22 +0530

    Time: 3 ms (52.39%), Space: 44.6 MB (56.61%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,31 +1,30 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        for (int i = 0; i < 9; i++) {
-            HashSet<Character> rows = new HashSet<>();
-            HashSet<Character> cols = new HashSet<>();
-            HashSet<Character> box = new HashSet<>();
-
-            for (int j = 0; j < 9; j++) {
-                // Row check
-                if (board[i][j] != '.') {
-                    if (rows.contains(board[i][j])) return false;
-                    rows.add(board[i][j]);
+        for(int i=0; i<9; i++){
+            HashSet<Character>row=new HashSet<>();
+            HashSet<Character>col=new HashSet<>();
+            HashSet<Character>curr=new HashSet<>();
+            
+            for(int j=0; j<9; j++){
+                //rowcheck
+                if(board[i][j]!='.'){
+                    if(row.contains(board[i][j])) return false;
+                    row.add(board[i][j]);
+                }
+                //colcheck
+                if(board[j][i]!='.'){
+                    if(col.contains(board[j][i])) return false;
+                    col.add(board[j][i]);
+                }
+                //grid check
+                int rowidx = 3*(i/3);
+                int colidx = 3*(i%3);
+                char c=board[rowidx + j/3][colidx + j%3];
+                if(c!='.'){
+                    if(curr.contains(c)) return false;
+                    curr.add(c);
                 }
 
-                // Column check
-                if (board[j][i] != '.') {
-                    if (cols.contains(board[j][i])) return false;
-                    cols.add(board[j][i]);
-                }
-
-                // Box check
-                int rowIndex = 3 * (i / 3);
-                int colIndex = 3 * (i % 3);
-                char current = board[rowIndex + j / 3][colIndex + j % 3];
-                if (current != '.') {
-                    if (box.contains(current)) return false;
-                    box.add(current);
-                }
             }
         }
         return true;
