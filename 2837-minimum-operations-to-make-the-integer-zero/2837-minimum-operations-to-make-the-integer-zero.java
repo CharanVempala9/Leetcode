8b9c58eb3fdfaa434f51772e88cf8c775aa00bef
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-09-05 21:27:59 +0530

    Time: 0 ms (100%), Space: 40.8 MB (64.62%) - LeetHub

--- 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.java
+++ 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.java
@@ -1,23 +1,16 @@
-import java.util.*;
-
 class Solution {
-    Map<String, Integer> dp = new HashMap<>();
-
-    private int solve(long num1, long num2, int k) {
-        String key = num1 + "," + k;
-        if (dp.containsKey(key)) return dp.get(key);
-
-        long m = num1 - k * num2;
-        if (m < k) return (int)1e9; 
-        if (Long.bitCount(m) <= k) return k;
-        int ans = solve(num1, num2, k + 1);
-
-        dp.put(key, ans);
-        return ans;
-    }
 
     public int makeTheIntegerZero(int num1, int num2) {
-        int ans = solve(num1, num2, 1);
-        return ans >= 1e9 ? -1 : ans;
+        int k = 1;
+        while (true) {
+            long x = num1 - (long) num2 * k;
+            if (x < k) {
+                return -1;
+            }
+            if (k >= Long.bitCount(x)) {
+                return k;
+            }
+            k++;
+        }
     }
 }
