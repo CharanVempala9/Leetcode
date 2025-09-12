12924c7a99d6ecbad8e5c5a995ec383ce3ca1eb0
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-09-12 15:21:33 +0530

    Time: 17 ms (17.85%), Space: 45.7 MB (30.18%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -1,23 +1,25 @@
 class Solution {
     public boolean doesAliceWin(String s) {
-        ArrayList<Integer>ls=new ArrayList<>();
+        int maxOdd=0;
         int cnt=0;
         for(int i=0; i<s.length(); i++){
             if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u'){
                 cnt++;
             }
-            ls.add(cnt);
+            if(cnt%2==1)
+            maxOdd=Math.max(cnt,maxOdd);
         }
         if(cnt==0) return false;
-        int len=ls.size()-1;
-        while(len>=0){
-            int Vcnt=ls.get(len);
-            if(Vcnt%2==1){
-                cnt-=Vcnt;
-                break;
-            }
-            len--;
-        }
+        // int len=ls.size()-1;
+        // while(len>=0){
+        //     int Vcnt=ls.get(len);
+        //     if(Vcnt%2==1){
+        //         cnt-=Vcnt;
+        //         break;
+        //     }
+        //     len--;
+        // }
+        cnt-=maxOdd;
         if(cnt!=0 && cnt%2==0) return false;
         return true; 
     }
