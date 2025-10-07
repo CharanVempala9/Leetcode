54d1b5f83ffaad7d26b0d528b47b5604e9aa0bc6
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-10-07 22:04:50 +0530

    Time: 64 ms (88.13%), Space: 62.2 MB (46.58%) - LeetHub

--- 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.java
+++ 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.java
@@ -2,31 +2,27 @@ import java.util.*;
 class Solution {
     public int[] avoidFlood(int[] rains) {
         int n = rains.length;
-        Map<Integer, Integer> lake = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>(); 
+        TreeSet<Integer> dryDays = new TreeSet<>();
+        int[] ans = new int[n];
+        Arrays.fill(ans, 1);                 
         for (int i = 0; i < n; i++) {
-            if (rains[i] != 0) {
-                if (lake.containsKey(rains[i])) {
-                    int pIdx = lake.get(rains[i]);
-                    boolean find = false;
-                    for (int j = pIdx; j < i; j++) {
-                        if (rains[j] == 0) {
-                            rains[j] = rains[i];
-                            find = true;
-                            break;
-                        }
+            int lake = rains[i];
+            if (lake == 0) {
+                dryDays.add(i);                   
+            } else {
+                ans[i] = -1;                         
+                if (map.containsKey(lake)) {
+                    Integer dryDay = dryDays.higher(map.get(lake));  
+                    if (dryDay == null) {
+                        return new int[0];
                     }
-                    if (!find) return new int[0];
-                    lake.put(rains[i], i);
-                    rains[i] = -1;
-                } else {
-                    lake.put(rains[i], i);
-                    rains[i] = -1;
+                    ans[dryDay] = lake;     
+                    dryDays.remove(dryDay);
                 }
+                map.put(lake, i);   
             }
         }
-        for (int i = 0; i < n; i++) {
-            if (rains[i] == 0) rains[i] = 1;
-        }
-        return rains;
+        return ans;
     }
 }
