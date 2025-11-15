6ae978ce4e337e73a2c8dc155035e44f3d44a60e
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-11-15 10:47:38 +0530

    Time: 20 ms (57.47%), Space: 77.1 MB (20.36%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.java
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.java
@@ -7,9 +7,13 @@ class Solution {
             int er=q[2];
             int ec=q[3];
             for(int i=sr; i<=er; i++){
-                for(int j=sc; j<=ec; j++){
-                    res[i][j]+=1;
-                }
+                res[i][sc]+=1;
+                if(ec+1<n)res[i][ec+1]-=1;
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=1; j<n; j++){
+                res[i][j]+=res[i][j-1];
             }
         }
         return res;
