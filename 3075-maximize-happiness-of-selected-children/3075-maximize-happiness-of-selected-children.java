45702c536741c00be4b8ff0a66509dc408c33c0f
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-12-25 10:52:02 +0530

    Time: 223 ms (5.38%), Space: 122.3 MB (6.45%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        int n=happiness.length;
+        long ans=0;
+        PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(int h:happiness){
+            pq.add(h);
+        }
+        int idx=0;
+        while(k-->0){
+            int v=pq.poll();
+            if(v-idx>=0)
+            ans+=v-idx;
+            idx++;
+        }
+        return ans;
+    }
+}
