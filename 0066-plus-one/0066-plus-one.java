b655ce880ea040bb538e7407c1b5758307824255
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2026-01-01 11:50:47 +0530

    Time: 0 ms (100%), Space: 43.3 MB (58.45%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,15 +1,30 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n = digits.length;
-        for (int i = n - 1; i >= 0; i--) {
-            if (digits[i] < 9) {  
-                digits[i]++;  
-                return digits;
+        int n=digits.length;
+        ArrayList<Integer>ls=new ArrayList<>();
+        int c=0;
+        for(int i=n-1; i>=0; i--){
+            int tmp=-1;
+            if(i==n-1){
+                tmp=digits[i]+1;
             }
-            digits[i] = 0;  
+            else{
+                tmp=digits[i]+c;
+            }
+            if(tmp>9){
+                ls.add(0);
+                c=1;
+            }
+            else{
+                ls.add(tmp);
+                c=0;
+            }
         }
-        int[] newDigits = new int[n + 1];
-        newDigits[0] = 1;
-        return newDigits;
+        ls.add(c);
+        int[] res=new int[n+c];
+        for(int i=n+c-1; i>=0; i--){
+            res[n+c-i-1]=ls.get(i);
+        }
+        return res;
     }
 }
