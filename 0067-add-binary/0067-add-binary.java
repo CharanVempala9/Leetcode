8f7af0bc7d842f4b68f3eff56ba7050531653d58
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2026-02-15 19:27:11 +0530

    Time: 1 ms (99.65%), Space: 43.8 MB (38.26%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,18 +1,72 @@
 class Solution {
     public String addBinary(String a, String b) {
-        int i=a.length()-1;
-        int j=b.length()-1;
+        int i1=a.length()-1;
+        int i2=b.length()-1;
         StringBuilder res=new StringBuilder();
         int carry=0;
-        while(i>=0 || j>=0 || carry==1){
-            if(i>=0)
-            carry+=a.charAt(i--)-'0';
-            if(j>=0)
-            carry+=b.charAt(j--)-'0';
-            res.append(carry%2);
-            carry/=2;
+        while(i1>=0 && i2>=0){
+            if(a.charAt(i1)=='1' && b.charAt(i2)=='1'){
+                if(carry==1){
+                    res.append('1');
+                }
+                else{
+                    res.append('0');
+                }
+                carry=1;
+            }
+            else if(a.charAt(i1)=='1' || b.charAt(i2)=='1'){
+                if(carry==1){
+                    res.append('0');
+                    carry=1;
+                }
+                else{
+                res.append('1');
+                }
+            }
+            else{
+                if(carry==1){
+                    res.append('1');
+                    carry=0;
+                }
+                else
+                res.append('0');
+            }
+            i1--;
+            i2--;
+        }
+        while(i1>=0){
+            char ch=a.charAt(i1);
+            if(ch=='1' && carry==1){
+                res.append('0');
+                carry=1;
+            }
+            else if(ch=='1' || carry==1){
+                res.append('1');
+                carry=0;
+            }
+            else{
+                res.append('0');
+            }
+            i1--;
+        }
+        while(i2>=0){
+            char ch=b.charAt(i2);
+            if(ch=='1' && carry==1){
+                res.append('0');
+                carry=1;
+            }
+            else if(ch=='1' || carry==1){
+                res.append('1');
+                carry=0;
+            }
+            else{
+                res.append('0');
+            }
+            i2--;
+        }
+        if(carry==1){
+            res.append('1');
         }
         return res.reverse().toString();
     }
-    
 }
